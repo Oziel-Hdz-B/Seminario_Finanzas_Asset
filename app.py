008aeadb82e3a996f2f873d915f001fea5ff3232
Oziel-Hdz-B <ozielhb@gmail.com>
2025-11-24 20:57:28 -0600

    cambio 1

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-print("Hola mundo!")
+import streamlit as st
+
+# Sidebar
+with st.sidebar:
+    # Información del proyecto
+    st.title("⚙️ Proyecto 1")
+    st.markdown('<div style="margin-top: 20px;"></div>', unsafe_allow_html=True)
+    st.markdown("""
+
+    **👥 Integrantes:**
+    - Oziel Hernández
+    - Daniela Borzani
+    - Santiago Cruz
+    - Ximena Paredes
+    """)
+    st.markdown("---")
