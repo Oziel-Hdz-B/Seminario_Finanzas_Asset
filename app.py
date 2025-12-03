1bb6f0aaab905fea398486dcdd9663be2cdbca00
Oziel-Hdz-B <ozielhb@gmail.com>
2025-12-03 16:15:51 -0600

    cambio 1 3 dic

--- app.py
+++ app.py
@@ -4,6 +4,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import plotly.express as px
 from datetime import datetime
+import yfinance as yf
+
+# #############################################################################
+# ##################################
+# CERO PARTE, SELECCIONAR ACTIVOS, FECHAS, Y MOSTRAR CARÁTULA
+# ##################################
+# #############################################################################
 
 # Sidebar
 with st.sidebar:
@@ -31,21 +38,21 @@ with st.sidebar:
     if str(selectbox_1) == "Regionales":
         ## Multiselect
         st.subheader("Activos por Regiones")
-        tickers_regiones = st.multiselect(
+        tickers = st.multiselect(
             label="Multiselect 1",
             options=[
-            'SPLG',  # Tecnología
-            'EWC',  # Finanzas
-            'IEUR',  # Salud
-            'EEM',  # Consumo básico
-            'EWJ',  # Consumo discrecional
+            'SPLG',  # 
+            'EWC',  # 
+            'IEUR',  # 
+            'EEM',  # 
+            'EWJ',  # 
             ],
             default=["SPLG"]
         )
     else:
         ## Multiselect
         st.subheader("Activos por Sectores")
-        tickers_sectores = st.multiselect(
+        tickers = st.multiselect(
             label="Multiselect 2",
             options=[
             'XLK',  # Tecnología
@@ -69,8 +76,8 @@ with st.sidebar:
     ## Date input
     st.subheader("Fechas")
     fecha_hoy = datetime.today()
-    fecha_min = datetime(year=2000, month=1, day=1)
-    fecha_max = fecha_hoy
+    fecha_min = datetime(year=2010, month=1, day=1)
+    fecha_max = datetime(year=2025, month=12, day=2)
     fecha_inicio = st.date_input(
         "Input Fecha de incio",
         fecha_hoy,
@@ -87,3 +94,81 @@ with st.sidebar:
     fecha_fin = datetime(year=fecha_fin.year, month=fecha_fin.month, day=fecha_fin.day)
 
     st.markdown("---")
+
+"""
+# Descarga de datos para todos los tickers
+@st.cache_data
+def obtener_datos (stocks):
+    df = yf.download(stocks, 
+                     start=datetime(year = 2010, 
+                                  month = 1, 
+                                  day = 1),
+                     end=datetime(year = datetime.today().year, 
+                                  month =datetime.today().month, 
+                                  day = datetime.today().day ))
+    
+df_regiones = obtener_datos(['SPLG','EWC','IEUR','EEM','EWJ'])
+df_sectores = obtener_datos(['XLK','XLF','XLV','XLY','XLE','XLI','XLC','XLB','XLU','XLRE'])
+
+st.write(pd.DataFrame(df_sectores))
+st.write(pd.DataFrame(df_regiones))
+
+df_sectores = pd.DataFrame(df_sectores).dropna(how="all",axis=1).dropna(how="any")
+st.write(df_sectores)
+"""
+
+# #############################################################################
+# ##################################
+# 1RA PARTE, SELECCIONAR EL TIPO DE PORTAFOLIO
+# ##################################
+# #############################################################################
+
+st.header("Escoge el tipo de portafolio que gustes")
+
+## Multiselect
+portafolio_tipo = st.selectbox(
+    "Tipos de portafolios",
+    [
+    'Portafolio arbitrario',  # Caso más simple con inputs de pesos
+    'Portafolio optimizado - Mínima Varianza',  # Markowitz Min Var
+    'Portafolio optimizado - Máximo Sharpe',  # Markowitz Max Sharpe Ratio
+    'Portafolio optimizado - Rendimiento Fijo',  # Markowitz con Rendimiento de input
+    'Portafolio optimizado - Black Litterman'
+    ],
+    index=1
+)
+
+# ###########################################################################
+# ##################################
+# 3ERA PARTE DIVIOSIÓN DE CASOS
+# ##################################
+# ###########################################################################
+
+if str(portafolio_tipo) == 'Portafolio arbitrario':
+    # ###########################################################################
+    # PRIMER CASO
+    # ###########################################################################
+    ## Tabla editable
+    st.subheader("Tabla de pesos, por favor elige los pesos")
+    pesos = [0.1 for _ in tickers]
+    df_port = df_port = pd.DataFrame({
+        "Tickers": tickers,
+        "Pesos": [0.1] * len(tickers)
+        })
+    tabla5 = st.data_editor(df_port, disabled=["Tickers"])
+
+elif str(portafolio_tipo) == 'Portafolio optimizado - Mínima Varianza':
+    # ###########################################################################
+    # SEGUNDO CASO
+    # ###########################################################################
+    pass
+elif str(portafolio_tipo) == 'Portafolio optimizado - Máximo Sharpe':
+    # ###########################################################################
+    # TERCER CASO
+    # ###########################################################################
+    pass
+elif str(portafolio_tipo) == 'Portafolio optimizado - Black Litterman':
+    # ###########################################################################
+    # QUINTO CASO
+    # ###########################################################################
+    pass
